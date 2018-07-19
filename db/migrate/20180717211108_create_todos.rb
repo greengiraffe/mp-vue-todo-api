a3a700862a023d384099dcafb3c5c124696c7495
Valentin von Guttenberg <greengiraffe@posteo.de>
2018-07-19 15:41:44 +0200

    Make category in todo optional

--- db/migrate/20180717211108_create_todos.rb
+++ db/migrate/20180717211108_create_todos.rb
@@ -5,7 +5,7 @@ class CreateTodos < ActiveRecord::Migration[5.2]
       t.datetime :due
       t.boolean :important
       t.boolean :done
-      t.references :category, foreign_key: true
+      t.references :category, foreign_key: true, optional: true
 
       t.timestamps
     end
